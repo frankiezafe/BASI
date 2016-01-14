d23a913e974e8d15a9c5e1b99c196e8acad4fe63
oceduse <aureliebeghin@hotmail.fr>
2016-01-14 13:39:03 +0100

    basi_kinect4
    ajout de sliders

--- java/Basi_kinect/src/BasiKinect.java
+++ java/Basi_kinect/src/BasiKinect.java
@@ -2,13 +2,21 @@ import SimpleOpenNI.SimpleOpenNI;
 import ground.Ground;
 import processing.core.*;
 import SimpleOpenNI.*;
+import processing.controlP5.ControlP5;
+import processing.controlP5.Slider;
+import processing.controlP5.Toggle;
+import processing.core.*;
+import processing.opengl.PGraphicsOpenGL;
 
 public class BasiKinect extends PApplet {
 
 	private Ground ground;
 	private PVector view_rot;
 	private boolean pause;
+	private ControlP5 ui;
 	
+	private Slider ui_minz;
+	private Slider ui_maxz;
 
 	public void settings() {
 		size(1280, 720, P3D);
@@ -23,19 +31,29 @@ public class BasiKinect extends PApplet {
 		pause = false;
 		// ground.print();
 		
+		//partie slider
+		int sliderw = 300;
+		ui = new ControlP5 ( this);
+		int uix = 10;
+		int uiy = 10;
+		ui.addLabel("distance d'affichage", uix, uiy);
+		uiy += 15;
+		ui_minz = ui.addSlider( "ground.minz", 0, 2000, uix, uiy, sliderw, 10).setValue(ground.minz);
+		uiy += 15;
+		ui_maxz = ui.addSlider( "ground.maxz", 2000, 4000, uix, uiy, sliderw, 10).setValue(ground.maxz);
 
 	}
 
 	public void draw() {
-		
-		view_rot.x = mouseY * 0.1f;
-		view_rot.y = mouseX * 0.1f;
-//		view_rot.y = PI;
+		ground.minz = ui_minz.getValue();
+		ground.maxz = ui_maxz.getValue();
+//		view_rot.x = mouseY * 0.1f;
+//		view_rot.y = mouseX * 0.1f;
+		view_rot.y = PI;
 //		view_rot.x = -0.2f;
 //		view_rot.y = PI - 0.6f;
 //		view_rot.z = 0;
 		background( 100,100,100 );
-//		lights();
 //		pointLight(255,255,255,0,0,-2000);
 		// mon image doit tourner
 		pushMatrix();
